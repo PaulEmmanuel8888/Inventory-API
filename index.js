17e2fb11d7a595df7503790121eafea7e6754698
Emmanuel Paul <paulemmanuel8888@gmail.com>
2026-06-06 16:07:09 +0100

    Update get product/:id route to improve validation

--- index.js
+++ index.js
@@ -17,14 +17,25 @@ app.get("/products", async (req, res) => {
   res.status(200).json(allProducts);
 });
 app.get("/products/:id", async (req, res) => {
-  const id = req.params.id;
-  const product = await getProductById(id);
-  if (product.length < 1) {
-    return res.status(404).json({
-      message: "Product Not Found",
+  const id = Number(req.params.id);
+
+  if (isNaN(id) || id <= 0) {
+    return res.status(400).json({
+      message: "Invalid product ID",
     });
   }
-  return res.status(200).json(product);
+
+  const product = await getProductById(id);
+
+  if (!product) {
+    return res.status(404).json({
+      message: "Product not found",
+    });
+  }
+
+  return res.status(200).json({
+    product,
+  });
 });
 
 //Post Routes
@@ -68,6 +79,8 @@ app.post("/products", async (req, res) => {
     product,
   });
 });
+
+//
 
 app.listen(PORT, () => {
   console.log(`Server running successfully at http://localhost:${PORT}/`);
